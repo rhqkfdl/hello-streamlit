a2113ba3fd2cf4af56f63b5702c9f46ae7c89d6a
rhqkfdl <108142788+rhqkfdl@users.noreply.github.com>
2024-03-27 00:55:47 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(np.random.randn(10,2), columns = ['x','y'])
-st.line_chart(df)
+st.sidebar.title("dfsfs")
+st.sidebar.button("www")
+st.sidebar.radio("aaaa",["sddd","dsad"])
