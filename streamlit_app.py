cb6fd3cc90c9185f9caedb40fc7bac5dc0e884b3
rhqkfdl <108142788+rhqkfdl@users.noreply.github.com>
2024-03-27 00:49:42 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-rand = np.random(np.random.normal(1,2,size=20))
+rand = np.random.normal(1,2,size=20)
 fig, ab = plt.subplots()
 ab.hist(rand,bins = 15)
 st.pyplot(fig)
